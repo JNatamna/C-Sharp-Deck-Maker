c9d4db380bd52030e51e8bdba55dee278f354b71
JNatamna <noor.atamna@gmail.com>
2020-10-28 16:28:03 +0100

    Test

---
+++ PG3302.CardGame/Card.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PG3302.CardGame
+{
+    class Card
+    {
+        //Create all suits in a deck of cards
+        public enum Suits
+        {
+            Hearts,
+            Spades,
+            Diamonds,
+            Clubs,
+            Joker
+        }
+
+        //Create ranks
+        public enum Ranks
+        {
+            Ace,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King,
+        }
+
+        public Suits suit;
+        public Ranks rank;
+
+        public Card(Ranks iRank, Suits iSuit)
+        {
+            suit = iSuit;
+            rank = iRank;
+        }
+    }
+}
--- PG3302.CardGame/CardDeck.cs
+++ PG3302.CardGame/CardDeck.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PG3302.CardGame
 {
-    class CardDeck
+    class CardDeck : Card
     {
+        public List<Card> Deck; //list of all playing cards
+
+        public void ArrangeDeck()
+        {
+            Deck = new List<Card>();
+            
+            foreach (Suits suit in Enum.GetValues(typeof(Suits)))
+            {
+                foreach (Ranks rank in Enum.GetValues(typeof(Ranks)))
+                {
+                    Deck.Add(new Card(rank, suit));
+                }
+            }
+        }
     }
 }
+
+    
+
